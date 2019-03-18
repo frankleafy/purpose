9f9fadb3b128c4b6a2eb54dd2f9ce6bb3dc58de9
frankleafy <leafy@nospoon.nl>
2019-03-18 22:49:35 +0100

    deploy

---
+++ precache-manifest.fe6dfe6e9738ba6c126bc831f0649d30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/purpose/robots.txt"
+  },
+  {
+    "revision": "b481acc781da09280082",
+    "url": "/purpose/js/chunk-vendors.2e4ab3f2.js"
+  },
+  {
+    "revision": "d3360623d6fd0c2a86a7",
+    "url": "/purpose/js/app.8bb985b3.js"
+  },
+  {
+    "revision": "26fee0059ec098d5f609",
+    "url": "/purpose/js/about.d288b4f1.js"
+  },
+  {
+    "revision": "d30c6515b812200789bccb24c3926b79",
+    "url": "/purpose/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/purpose/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d3360623d6fd0c2a86a7",
+    "url": "/purpose/css/app.84fe17e1.css"
+  }
+];
